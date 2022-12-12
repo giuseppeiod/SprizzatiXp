4a1e5ae1ff40724f1cf5e051b5fc4b963967fa8a
giuseppeiod <113522420+giuseppeiod@users.noreply.github.com>
2022-12-12 11:17:03 +0100

    Add files via upload

---
+++ NotiWeatherApp/NotiWeatherApp/NotiWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotiWeatherAppApp.swift
+//  NotiWeatherApp
+//
+//  Created by Giuseppe Iodice 2022/12/10.
+//
+
+import SwiftUI
+
+@main
+struct NotiWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
